3b41b9f7022962cdc2ba1e9ba5fbfcc14d945f20
ddddddeee <my.name.donut@hotmail.com>
2020-03-05 22:42:12 +0700

    change mysql to mariadb

--- ansible/template/phpinfodb.php
+++ ansible/template/phpinfodb.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = mysqli_connect("192.168.147.82", "arnut", "123456");
+$link = mysqli_connect("192.168.147.82", "myuser", "123456");
 
 if (mysqli_connect_errno()) {
   printf("Connect faild: %s\n", mysqli_connect_error());
@@ -8,5 +8,8 @@ if (mysqli_connect_errno()) {
 }
 printf("Server version %s\n", mysqli_get_server_info($link));
 mysqli_close($link);
-
-echo '\nCurrent PHP version: ' . phpversion();
+?>
+<br>
+<?php
+echo 'Current PHP version: ' . phpversion();
+?>
